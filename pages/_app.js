bae48623f4da88a3f4ceabb396443ced8cdf2e69
Joshua Pratheek <ajpratheek@gmail.com>
2026-06-01 05:55:12 +0530

    Removed custom cursor

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 
 import '../styles/globals.css';
-import Cursor from '../components/Cursor';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Cursor />
       <Navbar />
       <Component {...pageProps} />
       <Footer />
